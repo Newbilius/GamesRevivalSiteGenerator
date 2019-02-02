43a95cdfcfd8a4a6963bf6563af57c64e86df448
newbilius <newbilius@gmail.com>
2019-02-02 16:23:45 +0500

    Добавил страницу 404

---
+++ src/main/java/newbilius/GamesRevival/Generators/Error404PageGenerator.java
@@ -0,0 +1,43 @@
+package newbilius.GamesRevival.Generators;
+
+import newbilius.GamesRevival.FileHelper;
+import newbilius.GamesRevival.FoldersConfig;
+import newbilius.GamesRevival.GeneratorsInfastructure.BaseOnePageGenerator;
+import newbilius.GamesRevival.HTML.Breadcrumbs.MainBreadcrumbGenerator;
+import newbilius.GamesRevival.HTML.MDToHtmlConverter;
+
+import java.io.IOException;
+
+@SuppressWarnings("unused")
+public class Error404PageGenerator extends BaseOnePageGenerator {
+
+    public Error404PageGenerator(FoldersConfig foldersConfig) {
+        super(foldersConfig);
+    }
+
+    @Override
+    protected String getFileName() {
+        return "error404.html";
+    }
+
+    @Override
+    protected String getJSFileName() {
+        return "";
+    }
+
+    @Override
+    protected String getContent() throws IOException {
+        var builder = new StringBuilder();
+
+        var mdToHtmlConverter = new MDToHtmlConverter();
+        var faqData = FileHelper.getFileText(foldersConfig.getInputFolder() + "error404.md");
+        var text = mdToHtmlConverter.convert(faqData);
+        builder.append(text);
+        return builder.toString();
+    }
+
+    @Override
+    protected String getTitle() {
+        return "Games Revival - FAQ";
+    }
+}
