1f4371a97475d18ec770126f2393ef18d2ee743a
newbilius <newbilius@gmail.com>
2019-02-09 11:39:25 +0500

    Передалка списка игр - теперь там отображается краткий список портов

--- src/main/java/newbilius/GamesRevival/Generators/GamesListPageGenerator.java
+++ src/main/java/newbilius/GamesRevival/Generators/GamesListPageGenerator.java
@@ -1,5 +1,6 @@
 package newbilius.GamesRevival.Generators;
 
+import newbilius.GamesRevival.Data.Port;
 import newbilius.GamesRevival.FileHelper;
 import newbilius.GamesRevival.FoldersConfig;
 import newbilius.GamesRevival.GeneratorsInfastructure.BaseOnePageGenerator;
@@ -8,6 +9,7 @@ import newbilius.GamesRevival.HTML.Breadcrumbs.MainBreadcrumbGenerator;
 import newbilius.GamesRevival.HTML.MDToHtmlConverter;
 
 import java.io.IOException;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class GamesListPageGenerator extends BaseOnePageGenerator {
@@ -30,12 +32,15 @@ public class GamesListPageGenerator extends BaseOnePageGenerator {
     protected String getContent() throws IOException {
         var builder = new StringBuilder();
         for (var game : Games) {
-            builder.append(String.format("<tr class=\"game_line\"><td>" +
+            builder.append(String.format("<tr class=\"game_line\">" +
+                            "<td style=\"min-width:200px\">" +
                             "<a href=%s>%s</a></td>" +
                             "<td>%s</td></tr>",
                     RelativePathHelper.getPath(game),
                     game.getTitle(),
-                    game.Ports.size()));
+                    getModsList(game.Ports)
+                    )
+            );
         }
 
         var breadcrumbGenerator = new MainBreadcrumbGenerator();
@@ -44,6 +49,14 @@ public class GamesListPageGenerator extends BaseOnePageGenerator {
         return getTemplateFileContent("games_list.html")
                 .replace("#PAGE_TOP#", breadcrumbGenerator.generate())
                 .replace("#TABLE#", builder.toString());
+    }
+
+    private String getModsList(List<Port> ports) {
+        var portsString = ports
+                .stream()
+                .map(port -> port.Title.replace(" ", "&nbsp;"))
+                .toArray(String[]::new);
+        return String.join(", ", portsString);
     }
 
     @Override
