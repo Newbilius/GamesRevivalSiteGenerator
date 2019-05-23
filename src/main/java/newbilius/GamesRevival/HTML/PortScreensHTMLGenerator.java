87a514442762a2aef05bee38b9ac94fa17a205a9
Ilya Korobtsev <ikorobtsev@yandex-team.ru>
2019-05-24 00:26:55 +0300

    [fix] поправлена попытка escape текста

--- src/main/java/newbilius/GamesRevival/HTML/PortScreensHTMLGenerator.java
+++ src/main/java/newbilius/GamesRevival/HTML/PortScreensHTMLGenerator.java
@@ -6,6 +6,8 @@ import newbilius.GamesRevival.GeneratorsInfrastructure.RelativePathHelper;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static com.google.common.html.HtmlEscapers.htmlEscaper;
+
 public class PortScreensHTMLGenerator {
 
     public static String getImageUrl(Port port, String imagePathFromPort) {
@@ -17,7 +19,7 @@ public class PortScreensHTMLGenerator {
         var imageUrl = getImageUrl(port, imagePath);
         return String.format("<a data-title='%s' data-toggle='lightbox' data-gallery='example-gallery' target=_new href='%s'>" +
                         "<img src='%s' style='max-width:300px;margin:4px;' class='img-thumbnail img-fluid'></a>",
-                port.Title.replace("'", "\'"),
+                htmlEscaper().escape(port.Title),
                 imageUrl,
                 imageUrl
         );
