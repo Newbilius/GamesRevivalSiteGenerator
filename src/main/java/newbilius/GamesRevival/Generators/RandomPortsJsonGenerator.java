77b62571e93c80c9c1760ccb605271daa80712d7
newbilius <newbilius@gmail.com>
2019-08-18 11:51:30 +0500

    В блоке с рандомными модами теперь будет выводиться имя игры, а не порта

--- src/main/java/newbilius/GamesRevival/Generators/RandomPortsJsonGenerator.java
+++ src/main/java/newbilius/GamesRevival/Generators/RandomPortsJsonGenerator.java
@@ -1,6 +1,8 @@
 package newbilius.GamesRevival.Generators;
 
 import com.google.gson.Gson;
+import newbilius.GamesRevival.Data.Game;
+import newbilius.GamesRevival.Data.Port;
 import newbilius.GamesRevival.FileHelper;
 import newbilius.GamesRevival.FoldersConfig;
 import newbilius.GamesRevival.GeneratorsInfrastructure.BasePagesGenerator;
@@ -10,6 +12,7 @@ import newbilius.GamesRevival.HTML.PortScreensHTMLGenerator;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public class RandomPortsJsonGenerator extends BasePagesGenerator {
@@ -22,21 +25,39 @@ public class RandomPortsJsonGenerator extends BasePagesGenerator {
     protected void generateData() throws IOException {
         var ports = Games
                 .stream()
-                .flatMap(game -> game.Ports.stream())
-                .filter(port -> port.ImagesPath.length > 0)
+                .flatMap(this::CreatePortWithGame)
+                .filter(portWithGame -> portWithGame.Port.ImagesPath.length > 0)
                 .collect(Collectors.toList());
+
         Collections.shuffle(ports, new Random());
 
         var randomPorts = ports.stream()
                 .limit(300)
-                .map(port -> new RandomPortsJsonGenerator.RandomGame(port.Title,
-                        "/" + RelativePathHelper.getPath(port),
-                        PortScreensHTMLGenerator.getImageUrl(port, port.ImagesPath[0])
+                .map(portWithGame -> new RandomPortsJsonGenerator.RandomGame(portWithGame.Game.getTitle(),
+                        "/" + RelativePathHelper.getPath(portWithGame.Port),
+                        PortScreensHTMLGenerator.getImageUrl(portWithGame.Port, portWithGame.Port.ImagesPath[0])
                 ))
                 .toArray(RandomGame[]::new);
 
         var json = new Gson().toJson(randomPorts);
         FileHelper.writeStringToFile(foldersConfig.getOutputFolder() + "random_ports.json", json);
+    }
+
+    private Stream<PortWithGame> CreatePortWithGame(Game game) {
+        return game
+                .Ports
+                .stream()
+                .map(port -> new PortWithGame(game, port));
+    }
+
+    class PortWithGame {
+        Game Game;
+        newbilius.GamesRevival.Data.Port Port;
+
+        PortWithGame(newbilius.GamesRevival.Data.Game game, Port port) {
+            Game = game;
+            Port = port;
+        }
     }
 
     private class RandomGame {
@@ -45,8 +66,8 @@ public class RandomPortsJsonGenerator extends BasePagesGenerator {
         String ImgLink;
 
         RandomGame(String title,
-                          String url,
-                          String imageLink) {
+                   String url,
+                   String imageLink) {
             Title = title;
             Url = url;
             ImgLink = imageLink;
